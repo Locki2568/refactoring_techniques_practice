4b0cce68adfc13a41edbc74ae4fb212d9ec768a3
Locki Lo <locki2568@gmail.com>
2018-11-26 23:17:46 +0800

    Extracted printDetails, printBanner and calculateOutstanding as stand lone method

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -6,53 +6,58 @@ import java.util.List;
 import java.util.Vector;
 
 public class OwingPrinter {
-//    void printOwing(String name, List<Order> orders) {
+    void printOwing(String name, List<Order> orders) {
+        Iterator<Order> elements = orders.iterator();
+
+        printBanner();
+
+        double outstanding = calculateOutstanding(elements);
+
+        printDetails(name, outstanding);
+    }
+
+    private void printDetails(String name,Double outstanding){
+        // print details
+        System.out.println("name: " + name);
+        System.out.println("amount: " + outstanding);
+    }
+
+    private void printBanner(){
+        // print banner
+        final String banner = "*****************************\n"+
+                                "****** Customer totals ******\n"+
+                                "*****************************";
+        System.out.println (banner);
+    }
+
+    private double calculateOutstanding(Iterator<Order> elements){
+        // print owings
+        double outstanding = 0.0;
+        while (elements.hasNext()) {
+            Order each = (Order) elements.next();
+            outstanding += each.getAmount();
+        }
+        return outstanding;
+    }
+
+//     void printOwing(String name, List<Order> orders) {
 //        Iterator<Order> elements = orders.iterator();
 //        double outstanding = 0.0;
 //
-//        printBanner();
+//        // print banner
+//        System.out.println("*****************************");
+//        System.out.println("****** Customer totals ******");
+//        System.out.println("*****************************");
+//
+//        // print owings
+//        while (elements.hasNext()) {
+//            Order each = (Order) elements.next();
+//            outstanding += each.getAmount();
+//        }
 //
 //        // print details
 //        System.out.println("name: " + name);
 //        System.out.println("amount: " + outstanding);
 //    }
-//
-//    private void printBanner(){
-//        // print banner
-//        final String banner = "*****************************\n"+
-//                                "****** Customer totals ******\n"+
-//                                "*****************************";
-//        System.out.println (banner);
-//    }
-//
-//    private double claculateOutstanding(Iterator<Order> elements){
-//        // print owings
-//        double outstanding = 0.0;
-//        while (elements.hasNext()) {
-//            Order each = (Order) elements.next();
-//            outstanding += each.getAmount();
-//        }
-//        return outstanding;
-//    }
-
-     void printOwing(String name, List<Order> orders) {
-        Iterator<Order> elements = orders.iterator();
-        double outstanding = 0.0;
-
-        // print banner
-        System.out.println("*****************************");
-        System.out.println("****** Customer totals ******");
-        System.out.println("*****************************");
-
-        // print owings
-        while (elements.hasNext()) {
-            Order each = (Order) elements.next();
-            outstanding += each.getAmount();
-        }
-
-        // print details
-        System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
-    }
 }
 
